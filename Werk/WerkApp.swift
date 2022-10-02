8cc330dcf3c9128c6861d1cc66121b336c977537
Shaquil Campbell <shaquilwashington@gmail.com>
2022-10-02 18:45:32 -0400

    Initial Commit

---
+++ Werk/WerkApp.swift
@@ -0,0 +1,17 @@
+//
+//  WerkApp.swift
+//  Werk
+//
+//  Created by Shaquil Campbell on 10/2/22.
+//
+
+import SwiftUI
+
+@main
+struct WerkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
