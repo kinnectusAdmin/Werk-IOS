c123fc9f36ebfb33f182283a69e3a55c0d1f1eba
erraticallyrational <shaquilkcampbell@gmail.com>
2022-12-08 20:48:49 -0500

    pretty much finished WorkoutHistoryViewModel

---
+++ WorkoutCreationView.swift
@@ -0,0 +1,20 @@
+//
+//  WorkoutCreationView.swift
+//  Werk
+//
+//  Created by Shaquil Campbell on 12/8/22.
+//
+
+import SwiftUI
+
+struct WorkoutCreationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WorkoutCreationView_Previews: PreviewProvider {
+    static var previews: some View {
+        WorkoutCreationView()
+    }
+}
