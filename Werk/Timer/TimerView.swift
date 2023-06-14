116f51abe905e956302079b5c845c3d8d29246a4
erraticallyrational <shaquilkcampbell@gmail.com>
2023-06-14 00:51:57 -0400

    created visual rep of elapsed time and remaining time for timer view along with the media player buttons

--- Werk/Timer/TimerView.swift
+++ Werk/Timer/TimerView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct TimerView: View {
     @Environment(\.presentationMode) var presentationMode
-    var viewModel:TimerViewModel = TimerViewModel(workout: WorkoutBlueprint.initial)
+    @ObservedObject var viewModel:TimerViewModel = TimerViewModel(workout: WorkoutBlueprint.initial)
     
     let interval: Int = 0
     
@@ -29,16 +29,16 @@ struct TimerView: View {
                             .padding(.all, 5)
                             .background(Color.black.opacity(0.0))
                             .clipShape(Circle())
-                    }
-
-                  
+                    }.disabled(!viewModel.isScreenLocked)
+                    
+                    
                     Spacer()
                     // workout name
                     Text("Workout Timer")
                         .font(.title3)
                         .fontWeight(.semibold)
                         .foregroundColor(.white)
-
+                    
                     
                     Spacer()
                     
@@ -54,89 +54,183 @@ struct TimerView: View {
                 }
                 
                 //time remaining in workout
-                Text("\(viewModel.timeRemaining)")
+                Text("\(viewModel.phaseTime)")
+                //should be duration of current block minus time elapsed
                     .font(.largeTitle)
                 //                    .onReceive(viewModel.$timer) { _ in
                 //                        if Int(viewModel.work)! > 0 {
                 //                            Int(viewModel.timeRemaining)  -= 1
                 //                        }
                 //                    }
-                Spacer()
-            }
-            HStack{
                 
-                // blocks user screen interaction
-                Button {
-                    viewModel.didPressLock()
-                } label: {
-                    ZStack {
-                        Circle()
-                            .stroke(Color.white, lineWidth: 2)
-                            .frame(width: 50, height: 50,alignment: .leading)
-                            .padding(10)
-                        Image(systemName: "lock")
+                HStack{
+                    // Goes back to the previous phase in workout
+                    Button {
+                    } label: {
+                        Image(systemName: "lessthan")
                             .resizable()
                             .aspectRatio(contentMode: .fit)
-                            .frame(width: 25, height: 25)
+                            .frame(width: 15, height: 15)
                             .padding()
                             .foregroundColor(.white)
                             .cornerRadius(10)
                     }
-                }.allowsHitTesting(viewModel.isScreenLocked)
-                
-                ZStack {
-                    // timer display circle
-                    Circle()
-                        .stroke(Color.gray, lineWidth: 5)
-                        .frame(width: 220, height: 220)
-                    // shows decrease in time over course of current phase
-                    Circle()
-                        .trim(from: 0, to: 1)
-                        .stroke(Color.white, lineWidth: 2.5)
-                        .frame(width: 220, height: 220)
-                        .rotationEffect(.degrees(90))
+                    Spacer()
                     
-                    // pause start button
+                    Text(viewModel.currentPhaseName)
+                    
+                    Spacer()
+                    
+                    //goes to the next phase in workout
                     Button {
-                        viewModel.didPressStartorResume()
+                    } label: {
+                        Image(systemName: "greaterthan")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 15, height: 15)
+                            .padding()
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
+                }
+                
+                
+                HStack{
+                    Spacer()
+                    //shows the total amont of time that has elapsed during the workout
+                    Text("\(viewModel.elapsedTime) \nElapsed")
+                        .multilineTextAlignment(.center)
+                    
+                    Spacer()
+                    //shows the total duration of the workout
+                    Text("\(viewModel.timeRemaining) \nRemaining")
+                        .multilineTextAlignment(.center)
+                    
+                    Spacer()
+                }
+                Spacer()
+                
+                HStack{
+                    
+                    // blocks user screen interaction
+                    Button {
+                        viewModel.didPressLock()
                     } label: {
                         ZStack {
                             Circle()
-                                .fill(Color.black)//Will remove after button code is set
-                                .opacity(0.2)
-                                .frame(width: 100, height: 100)
-                            Text(viewModel.isTimerActive ? "Pause" : "Start")
-                                .font(.headline)
+                                .stroke(Color.white, lineWidth: 2)
+                                .frame(width: 50, height: 50,alignment: .leading)
+                                .padding(10)
+                            Image(systemName: "lock")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 25, height: 25)
+                                .padding()
                                 .foregroundColor(.white)
+                                .cornerRadius(10)
                         }
                     }
+                    //.enable bind to bool
                     
-                }
-                //resets timer
-                Button {
-                    viewModel.didPressReset()
-                } label: {
                     ZStack {
+                        // timer display circle
                         Circle()
-                            .stroke(Color.white, lineWidth: 2)
-                            .frame(width: 50, height: 50,alignment: .leading)
-                            .padding(10)
-                        Image(systemName: "arrow.counterclockwise")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 25, height: 25)
-                            .padding()
-                            .foregroundColor(.white)
-                            .cornerRadius(10)
+                            .stroke(Color.gray, lineWidth: 5)
+                            .frame(width: 220, height: 220)
+                        // shows decrease in time over course of current phase
+                        Circle()
+                            .trim(from: 0, to: 1)
+                            .stroke(Color.white, lineWidth: 2.5)
+                            .frame(width: 220, height: 220)
+                            .rotationEffect(.degrees(90))
+                        
+                        //  start/resume button
+                        Button {
+                            viewModel.didPressStartorResume()
+                        } label: {
+                            ZStack {
+                                Circle()
+                                    .fill(Color.black)//Will remove after button code is set
+                                    .opacity(0.2)
+                                    .frame(width: 100, height: 100)
+                                Text(viewModel.isTimerActive ? "Resume" : "Start")
+                                    .font(.headline)
+                                    .foregroundColor(.white)
+                            }
+                        }.disabled(viewModel.isScreenLocked)
+                        
                     }
+                    //resets timer
+                    Button {
+                        viewModel.didPressReset()
+                    } label: {
+                        ZStack {
+                            Circle()
+                                .stroke(Color.white, lineWidth: 2)
+                                .frame(width: 50, height: 50,alignment: .leading)
+                                .padding(10)
+                            Image(systemName: "arrow.counterclockwise")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 25, height: 25)
+                                .padding()
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                        }
+                    }.disabled(viewModel.isScreenLocked)
+                }
+                
+                Spacer()
+                
+                
+                HStack{
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "backward.fill")
+                            .resizable()
+                            .frame(width: 30, height: 20)
+                    }
+                    .padding()
+                    .foregroundColor(.white)
+                    
+                    
+                    
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "play.fill")
+                            .resizable()
+                            .frame(width: 20, height: 20)
+                    }
+                    .foregroundColor(.white)
+                    
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "forward.fill")
+                            .resizable()
+                            .frame(width: 30, height: 20)
+                    }
+                    .padding()
+                    .foregroundColor(.white)
+                    
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "music.note.list")
+                            .resizable()
+                            .frame(width: 20, height: 20)
+                    }
+                    .padding()
+                    .foregroundColor(.white)
+
                 }
                 
             }
         }
     }
 }
-
-
 struct LowerTimerView_Previews: PreviewProvider {
     static var previews: some View {
         TimerView(viewModel: TimerViewModel(workout: WorkoutBlueprint.initial))
--- Werk/Timer/TimerViewModel.swift
+++ Werk/Timer/TimerViewModel.swift
@@ -8,21 +8,64 @@
 import Foundation
 import SwiftUI
 
-
+/*
+    WorkoutBlock x 10
+    Each block has its own individual planned Duration which does not change -> Constant
+    Each block also a time elapsed -> this changes -> as that specific block is active and time passes
+    Only one block will be active at a time.
+ 
+    the total of the entire workout
+    is comprised of the sum of all the workoutblocks PLANNED duration
+ 
+    the time elapsed is the sum of all the workout blocks timeElapsed
+ 
+    time remaining = (the sum of all the workoutblocks PLANNED duration) - (the sum of all the workout blocks timeElapsed)
+ 
+ 
+    0 1 2 3 4 5
+ */
 class TimerViewModel: ObservableObject {
     private var workout: WorkoutBlueprint!
-    var workoutBlocks: [WorkoutBlock] = []
+    @Published var workoutBlocks: [WorkoutBlock] = []
     var currentPhaseIndex: Int = 0
-    @Published var currentPhase:String = ""
+    @Published var currentPhaseName:String = ""
     @Published var setIndicator:String = ""
-    @Published var phaseTime:String = ""
-    @Published var elapsedTime:String = ""
-    @Published var timeRemaining:Int = 0
+    @Published var phaseTime:Int = 0
+    @Published var elapsedTime:Int = 0
+    private var totalPlannedDuration: Int = 90
+    var timeRemaining: Int {
+        abs(elapsedTime - totalPlannedDuration)
+    }
     @Published var isTimerActive:Bool = false
-    @State var isScreenLocked:Bool = false
+    @Published var isScreenLocked:Bool = false
+    
     
     init(workout: WorkoutBlueprint) {
         self.workout = workout
+        
+        //        $workoutBlocks.map { blocks in
+        //            blocks.map { $0.timeElapsed }.reduce(0, +)
+        //        }.map { [weak self] sum -> Int in
+        //            guard let self = self else { return 0 }
+        //            return self.totalPlannedDuration - sum
+        //        }.assign(to: &$elapsedTime)
+        
+        //        totalPlannedDuration = workoutBlocks.map { durations in
+        //            durations.plannedDuration}.reduce(0, +)
+        
+        
+
+       // phaseTime = workoutBlocks[0].plannedDuration  <- same here ---v
+        phaseTime = workoutBlocks.map{ phaseTime in
+            phaseTime.plannedDuration
+        }.first ?? 0
+        
+        //        currentPhaseName = workoutBlocks[currentPhaseIndex].name  <- why does this not work?
+        currentPhaseName = workoutBlocks.map{phaseName in
+            phaseName.name}.first ?? "nil"    //  <- is it because there are no workouts saved?
+        
+        
+        
         let cycleBlocks = workout.intervals.cycles.map { cycle in
             if cycle.order == .startsWithHighIntensity {
                 return (0...cycle.numberOfSets).map { cycleSet in
@@ -52,7 +95,7 @@ class TimerViewModel: ObservableObject {
         
     }
     
-    @State var timer:Timer
+    @State var timer:Timer?
     
     func didPressExit(){
         
@@ -64,36 +107,61 @@ class TimerViewModel: ObservableObject {
         //when the edit button is pressed this should bring up the saved workoutCreationView for this saved workout
     }
     
-    func didPressPreviousPhase() {
-        
-        //switches to the previous intensity phase but also resets the timer, time elaspsed and remaning time to the beginning of that phase
-    }
-    
     func didPressNextPhase() {
+        currentPhaseIndex += 1
+        if currentPhaseIndex >= workoutBlocks.count{
+            currentPhaseIndex = 0
+        }
+//        var selectedPhase = workoutBlocks[currentPhaseIndex]
+        var phaseDurations = workoutBlocks.map{ durations in
+            durations.plannedDuration
+        }
+        var totalDurations = 0
+        for index in (0...currentPhaseIndex){
+            let completedPhaseDuration = phaseDurations[index]
+            totalDurations += completedPhaseDuration
+            
+        }
         
         //switches to the next intensity phase but also adds the duration of the "swithced from" phase to the total time elapsed and subtracts it from the time remanning
     }
     
-    func didPressLock() {
-        Toggle("lock", isOn: $isScreenLocked)
+    func didPressPreviousPhase() {
+        currentPhaseIndex -= 1
+        if currentPhaseIndex < 0 {
+            currentPhaseIndex = 0
+        }
+        var selectedPhase = workoutBlocks[currentPhaseIndex]
+        var phaseDurations = workoutBlocks.map{ durations in
+            durations.plannedDuration
+        }
+        var totalDurations = 0
+        for index in (0...currentPhaseIndex){
+            let completedPhaseDuration = phaseDurations[index]
+            totalDurations += completedPhaseDuration
+            
+        }
         
-        //toggles user touch input
+        //switches to the previous intensity phase but also resets the timer, time elaspsed and remaning time to the beginning of that phase
     }
     
-    func didPressStartorResume()-> Int {
+    
+    func didPressLock() {
+        isScreenLocked = !isScreenLocked
+        //toggles user touch input should disable other buttons not lock entire screen
+    }
+    
+    func didPressStartorResume() {
         //starts or resumes timer
         isTimerActive.toggle()
-        timeRemaining = workoutBlocks.map { durations in
-            durations.plannedDuration
-        }.reduce(0, +)
         timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: false, block: { _ in
-            self.timeRemaining -= 1
+            self.elapsedTime += 1
         })
-        return timeRemaining
+        
     }
     
     func didPressStop() {
-        isTimerActive.toggle()
+        //        isTimerActive.toggle()
     }
     
     func didPressReset() {
@@ -101,8 +169,34 @@ class TimerViewModel: ObservableObject {
         
     }
     
-
+    
 }
-
-
 // FINISH THIS IF STUCK THEN GO ADD SET FUNCTION TO WORKOUT CREATION REMEMBER TO SWITCH BRANCHES !
+/*
+ 
+ [0, 1, 2, 3]
+
+ [Block 1, Block 2, Block 3, Block 4]
+ 
+ [Blocks].map. { $0.plannedDuration }
+ 
+ [Block 1 duration, Block 2 duration, Block 3 duration, Block 4 duration]
+ 
+ 
+ [Block 1, Block 2, Block 3].map { $0.plannedDuration }.reduce(0, +)
+
+ var totalDuration: Int = 0
+ for block in blocks1 through 3 {
+    totalDuration += block.plannedDuration
+ }
+ 
+ var totalDuration: Int = 0
+ for index in (0...currentPhaseIndex) {
+    let completedBlock = blocks[index]
+    totalDuration += completedBlock.plannedDuration
+ }
+ 
+ 
+ When the user hits the next or previous button what is the time elapsed on your timer
+ the timer has to reset to "0" which is the planned duration of the selceted phase
+ */
